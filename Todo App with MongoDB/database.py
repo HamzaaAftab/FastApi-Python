26182d368b1306f2f6276fc61feb17ec73ffa24b
HamzaaAftab <163124480+HamzaaAftab@users.noreply.github.com>
2025-03-18 14:04:18 +0500

    env secured

--- Todo App with MongoDB/database.py
+++ Todo App with MongoDB/database.py
@@ -1,7 +1,12 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_URI = "mongodb+srv://hamzaaftab992:hamzakhan@cluster0.cm5lq.mongodb.net"
-DATABASE_NAME = "todo_db"
+# Load environment variables from .env file
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
